966b7441b6a215a9e94e4b2e0ef80e9a3d722c77
jonashenrique90 <jonas.herique90@gmail.com>
2021-02-26 16:01:48 -0300

    Ajuste initialTime Countdown

--- src/hooks/CountdownContext.tsx
+++ src/hooks/CountdownContext.tsx
@@ -16,7 +16,7 @@ interface ChallengesProviderProps {
 }
 
 let countdownTimeout: NodeJS.Timeout;
-const initialTime = (0.1 * 60)
+const initialTime = (25 * 60)
 
 export const CountdownContext = createContext({} as CountdownContextData)
 
